1feaea01e300f0568b965e15332728810341c0c0
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-11-14 09:34:24 -0500

    ajout logique de connexion a la db

---
+++ db_config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "localhost";
+$password = "";
+$dbname = "ecom1_tp2";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("La connexion à la base de données a échoué : " . $conn->connect_error);
+}
+?>
