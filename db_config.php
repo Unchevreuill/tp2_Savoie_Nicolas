8b8d9fc844234deba40c6bbcb100ec1723daa53a
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-11-14 09:47:49 -0500

    correctif de connexion a la db

--- db_config.php
+++ db_config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "localhost";
-$username = "localhost";
+$username = "root";
 $password = "";
 $dbname = "ecom1_tp2";
 
--- process.php
+++ process.php
@@ -1,37 +1,38 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données d'adresse depuis le formulaire d'adresse
-    $streets = $_POST["street"];
-    $streetNumbers = $_POST["street_nb"];
-    $types = $_POST["type"];
-    $cities = $_POST["city"];
+    $streets = isset($_POST["street"]) ? $_POST["street"] : [];
+    $streetNumbers = isset($_POST["street_nb"]) ? $_POST["street_nb"] : [];
+    $types = isset($_POST["type"]) ? $_POST["type"] : [];
+    $cities = isset($_POST["city"]) ? $_POST["city"] : [];
 
-    //internal import
+    // Inclure le fichier de configuration de la base de données
     include_once("db_config.php");
 
-    
+    // Préparer la requête d'insertion
     $stmt = $conn->prepare("INSERT INTO adresse (street, street_nb, type, city) VALUES (?, ?, ?, ?)");
 
-    
+    // Liaison des paramètres
     $stmt->bind_param("siss", $street, $street_nb, $type, $city);
 
-    // for pour inserer mes adresse
+    // Boucle pour insérer chaque adresse
     for ($i = 0; $i < count($streets); $i++) {
         $street = $streets[$i];
         $street_nb = $streetNumbers[$i];
         $type = $types[$i];
         $city = $cities[$i];
 
-        // executer ma requete
+        // Exécution de la requête
         $stmt->execute();
     }
 
-    // Fermer mes connexions
+    // Fermer la connexion et le statement
     $stmt->close();
     $conn->close();
 
-    // Redirection vers verification.php
+    // Rediriger vers la page de vérification
     header("Location: verification.php");
     exit();
 }
 ?>
+
