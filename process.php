26fb25d75a658717a5dc8bc17c81a2ec1d442d95
Nicolas Savoie <nic-savoie@hotmail.fr>
2023-11-14 09:36:53 -0500

    modif process pour ajouter une logique de base de donner

--- process.php
+++ process.php
@@ -1,14 +1,37 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // recuperer le nb d'adresse
-    $addressCount = $_POST["addressCount"];
+    // Récupérer les données d'adresse depuis le formulaire d'adresse
+    $streets = $_POST["street"];
+    $streetNumbers = $_POST["street_nb"];
+    $types = $_POST["type"];
+    $cities = $_POST["city"];
 
-   //enregistrer le nb dans une variable session pour lutiliser plus tard
-    session_start();
-    $_SESSION["addressCount"] = $addressCount;
+    //internal import
+    include_once("db_config.php");
 
-    // redirection vers mon formulaire pour les adresses
-    header("Location: address_form.php");
+    
+    $stmt = $conn->prepare("INSERT INTO adresse (street, street_nb, type, city) VALUES (?, ?, ?, ?)");
+
+    
+    $stmt->bind_param("siss", $street, $street_nb, $type, $city);
+
+    // for pour inserer mes adresse
+    for ($i = 0; $i < count($streets); $i++) {
+        $street = $streets[$i];
+        $street_nb = $streetNumbers[$i];
+        $type = $types[$i];
+        $city = $cities[$i];
+
+        // executer ma requete
+        $stmt->execute();
+    }
+
+    // Fermer mes connexions
+    $stmt->close();
+    $conn->close();
+
+    // Redirection vers verification.php
+    header("Location: verification.php");
     exit();
 }
 ?>
